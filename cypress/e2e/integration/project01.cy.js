252371c08fac00f095c4279b633c409ac5ac584e
volkan.tutunculer <volkan.tutunculer@gmail.com>
2025-05-06 12:52:57 -0400

    final update for Homework :)

--- cypress/e2e/integration/project01.cy.js
+++ cypress/e2e/integration/project01.cy.js
@@ -37,6 +37,115 @@ describe("Cypress Homework 1", () => {
       .should("be.visible")
       .and("have.text", "Gender *");
 
-      cy.get('input[name="question"], input[class="mr-1"]').should('have.attr', 'required')
+    cy.get('input[name="question"], input[class="mr-1"]').should(
+      "have.attr",
+      "required"
+    );
+    cy.get("label.radio").eq(0).should("contain.text", "Male");
+    cy.get("label.radio").eq(1).should("contain.text", "Female");
+    cy.get("label.radio")
+      .eq(2)
+      .should("contain.text", "Prefer not to disclose");
+
+    cy.get('input[name="question"]').each(($el) => {
+      cy.wrap($el).should("be.visible").and("not.be.checked");
+    });
+
+    cy.get("label.radio").eq(0).click();
+    cy.get('input[name="question"]').eq(0).should("be.checked");
+    cy.get('input[name="question"]').eq(1).should("not.be.checked");
+    cy.get('input[name="question"]').eq(2).should("not.be.checked");
+
+    cy.get("label.radio").eq(1).click();
+    cy.get('input[name="question"]').eq(1).should("be.checked");
+    cy.get('input[name="question"]').eq(0).should("not.be.checked");
+    cy.get('input[name="question"]').eq(2).should("not.be.checked");
   });
+
+  it("Test Case 04 - Validate the Address input box", () => {
+    cy.get('label[class="label"]').contains("Address").should("be.visible");
+
+    cy.get('input[placeholder="Enter your address"]')
+      .should("be.visible")
+      .and("not.have.attr", "required");
+  });
+
+  it("Test Case 05 - Validate the Email input box", () => {
+    cy.get('label[class="label"]').contains("Email *").should("be.visible");
+
+    cy.get('input[type="email"]')
+      .should("be.visible")
+      .and("have.attr", "placeholder", "Enter your email")
+      .and("have.attr", "required");
+  });
+
+  it("Test Case 06 - Validate the Phone input box", () => {
+    cy.get('label[class="label"]').contains("Phone").should("be.visible");
+
+    cy.get('input[type="phone"]')
+      .should("be.visible")
+      .and("have.attr", "placeholder", "Enter your phone number")
+      .and("not.have.attr", "required");
+  });
+
+  it("Test Case 07 - Validate the Message text area", () => {
+    cy.get('label[class="label"]').contains("Message").should("be.visible");
+
+    cy.get('textarea[class="textarea"]')
+      .should("be.visible")
+      .and("have.attr", "placeholder", "Type your message here...")
+      .and("not.have.attr", "required");
+  });
+
+  it("Test Case 08 - Validate the Consent checkbox", () => {
+    cy.get('input[type="checkbox"]')
+      .should("be.visible")
+      .and("have.attr", "required");
+
+    cy.get('textarea[class="textarea"]').should("be.visible");
+
+    cy.get('label[class="checkbox"]').should(
+      "have.text",
+      " I give my consent to be contacted."
+    );
+
+    cy.get('input[type = "checkbox"]')
+      .check()
+      .should("be.checked")
+      .uncheck()
+      .should("not.be.checked");
+  });
+
+  it("Test Case 09 - Validate the SUBMIT button", () => {
+    cy.get('button[type="submit"]')
+      .should("be.visible")
+      .should("be.enabled")
+      .and("have.text", "SUBMIT");
+  });
+
+  it("Test Case 10 - Validate the form submission", () => {
+    let fullName = "Volkan Tutunculer";
+    let adress = "this is my address";
+    let email = "volkan@gmail.com";
+    let phone = "123 456 7890";
+    let message = "this is my a message for you";
+
+    cy.get('input[placeholder="Enter your full name"]').type(fullName);
+    cy.get("label.radio").eq(0).click();
+    cy.get('input[placeholder="Enter your address"]').type(adress);
+    cy.get('input[type="email"]').type(email);
+    cy.get('input[type="phone"]').type(phone);
+    cy.get('textarea[class="textarea"]').type(message);
+    cy.get('input[type = "checkbox"]').check();
+    cy.get('button[type="submit"]').click();
+    cy.get('strong[class="mt-5"]')
+      .should("be.visible")
+      .and("have.text", "Thanks for submitting!");
+
+    cy.on("uncaught:exception", () => {
+      return false;
+    });
+
+  });
+  
 });
