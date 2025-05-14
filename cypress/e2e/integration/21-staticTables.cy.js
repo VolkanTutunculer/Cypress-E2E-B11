538a69e54a6f2b6107feb98737551bf75eef90f8
volkan.tutunculer <volkan.tutunculer@gmail.com>
2025-05-13 21:17:45 -0400

    updates

--- cypress/e2e/integration/21-staticTables.cy.js
+++ cypress/e2e/integration/21-staticTables.cy.js
@@ -1,7 +1,15 @@
-describe("Static Tables", () => {
+import TablesPage from "../../pages/TablesPage";
+
+describe("Static Tables", { tags: "@regression" }, () => {
+  const tablesPage = new TablesPage();
+
   beforeEach(() => {
-    cy.visit(`${Cypress.env("SITE_URL")}/frontend`)
+    cy.visit(`${Cypress.env("SITE_URL")}/frontend`);
     cy.clickCard("Tables");
+
+    cy.fixture("example").then(function (data) {
+      this.headers = data.headers;
+    });
   });
 
   /**
@@ -11,7 +19,9 @@ describe("Static Tables", () => {
    * Click on the "Tables" card
    * Validate the headers of the table are "Rank", "Company", "Employees", and "Country"
    */
-  it("Verify the headers of the table", () => {
-    
+  it("Verify the headers of the table", { tags: "@table" }, function () {
+    tablesPage.getCompanyTableHeaders().each(($el, index) => {
+      cy.wrap($el).should("have.text", this.headers[index]);
+    });
   });
 });
